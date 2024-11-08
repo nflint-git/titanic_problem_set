c15fd9ca8d3779f5890380e6b889565619b1eae5
edwinyeungcam <edwinhoyeung@gmail.com>
2024-11-08 00:13:41 +0000

    Added all the graphs, create a directory to save the pngs, and create a new ipynb for analysis

--- pys/plot.py
+++ pys/plot.py
@@ -54,6 +54,25 @@ def dist_plot(df, columns, hue=None):
     for j in range(i + 1, len(axes)):
         f.delaxes(axes[j])
     plt.tight_layout()
+    plt.show()
+
+def plot_count_pairs(data_df, feature, title, graph_title, hue="set"):
+    color_list = ["#A5D7E8", "#576CBC", "#19376D", "#0b2447"]
+    f, ax = plt.subplots(1, 1, figsize=(8, 4))
+    sns.countplot(x=feature, data=data_df, hue=hue, palette= color_list)
+    plt.grid(color="black", linestyle="-.", linewidth=0.5, axis="y", which="major")
+    ax.set_title(f"Number of passengers / {title}")
+    plt.savefig(f"figures/{graph_title}.png")
     plt.show()  
+
+def plot_distribution_pairs(data_df, feature, title, graph_title, hue="set"):
+    color_list = ["#A5D7E8", "#576CBC", "#19376D", "#0b2447"]
+    f, ax = plt.subplots(1, 1, figsize=(8, 4))
+    for i, h in enumerate(data_df[hue].unique()):
+        g = sns.histplot(data_df.loc[data_df[hue]==h, feature], color=color_list[i], ax=ax, label=h)
+    ax.set_title(f"Number of passengers / {title}")
+    g.legend()
+    plt.savefig(f"figures/{graph_title}.png")
+    plt.show()
 
 
